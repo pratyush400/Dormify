ac97846a6ba5c24158e8ce3900bd6284ba621f1e
Pratyush <lc23-0429@lclark.edu>
2026-03-15 16:00:59 -0700

    added auto login

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,9 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB8HZVV0Z71bu-arFd52pwrAoQD-MIBgiw',
@@ -10,11 +12,18 @@ const firebaseConfig = {
   storageBucket: 'dormify-now.firebasestorage.app',
   messagingSenderId: '283928198587',
   appId: '1:283928198587:web:fe0bfac9fdcc5c90b9989e',
-  measurementId: "G-ZJE7BSN529"
+  measurementId: 'G-ZJE7BSN529',
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+
+export const auth = getApps().length === 0
+  ? initializeAuth(app, {
+      persistence: getReactNativePersistence(AsyncStorage),
+    })
+  : getAuth(app);
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
